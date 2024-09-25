366fa3619db3f5172c2dbbcc8c7462acd354eb15
Joseph Peters <petersjl@rose-hulman.edu>
2024-09-25 15:24:37 -0400

    Reduce noise in analog signal

--- StarterKitProjects/03-PulseWidthModulation/03-PulseWidthModulation.ino
+++ StarterKitProjects/03-PulseWidthModulation/03-PulseWidthModulation.ino
@@ -1,6 +1,10 @@
 int potpin=0;// initialize analog pin 0
 int ledpin=11;//initialize digital pin 11（PWM output）
 int val=0;// Temporarily store variables' value from the sensor
+int history[10];
+int history_size = sizeof(history) / sizeof(history[0]);
+int index = 0;
+int duty = 0;
 void setup()
 {
   pinMode(ledpin,OUTPUT);// define digital pin 11 as “output”
@@ -10,7 +14,13 @@ void setup()
 void loop()
 {
   val=analogRead(potpin);// read the analog value from the sensor and assign it to val
-  Serial.println(val);// display value of val
-  analogWrite(ledpin,val/4);// turn on LED and set up brightness（maximum output of PWM is 255）
+  //Serial.println(val);// display value of val
+  history[index++ % history_size] = val;
+  duty = 1023;
+  for (int i = 0; i < history_size; i++){
+    duty = min(history[i], duty);
+  }
+  Serial.println(duty);
+  analogWrite(ledpin,duty/4);// turn on LED and set up brightness（maximum output of PWM is 255）
   delay(10);// wait for 0.01 second
 }
